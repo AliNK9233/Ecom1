79538be9b79ab59ef9f81bfbb30c38d8271c2b4c
Ali Akbar <nkali3@outlook.com>
2024-02-29 01:25:00 +0530

    hosting STATICFILES_DIRS removed due to conflict

--- Ecom_project/settings.py
+++ Ecom_project/settings.py
@@ -142,10 +142,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
     
-]
+# ]
 
 STATIC_ROOT = '/home/ubuntu/project/ecom1/static'
 
